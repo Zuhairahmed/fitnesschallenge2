559c93d9144e7a3d0372493091da56fb713bb7de
Zuhairahmed <ahmedz@oregonstate.edu>
2021-07-16 19:30:29 -0700

    new app

--- constants.py
+++
@@ -1,6 +0,0 @@
-login = "login"
-pantry = "pantry"
-recipes = "recipes"
-register = "register"
-users = "users"
-http_verbs = ['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE']
